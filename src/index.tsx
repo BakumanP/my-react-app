0ce53abad646f163215c4163d555c43c8e374ceb
Molyp <lifeng9797@gmail.com>
2020-12-07 23:22:07 +0800

    无用修改

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from '@/App';
 
 if ((module as any).hot) (module as any).hot.accept();
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App name="hello" />, document.getElementById('root'));
